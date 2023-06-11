455b7c7dfd7e3e36ddef1423a0565ff8935a3ee5
Layne <isslyne@gmail.com>
2023-06-12 00:58:45 +0800

    feat: latest

--- scripts/config.termux.sh
+++ scripts/config.termux.sh
@@ -2,30 +2,39 @@
 set -xe
 
 apt update
-apt install -y git cmake clang pkg-config libssl-dev
-apt install -y libevent libcurl openssl pcre2 python2
+apt install -y git cmake clang pkg-config
+apt install -y libevent libcurl openssl pcre2
 
 git clone https://github.com/jbeder/yaml-cpp --depth=1
 cd yaml-cpp
-cmake -DCMAKE_INSTALL_PREFIX=/data/data/com.termux/files/usr -DYAML_CPP_BUILD_TESTS=OFF -DYAML_CPP_BUILD_TOOLS=OFF .
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DYAML_CPP_BUILD_TESTS=OFF -DYAML_CPP_BUILD_TOOLS=OFF .
 make install -j3
 cd ..
 
 git clone https://github.com/tencent/rapidjson --depth=1
 cd rapidjson
-cp -r include/* /data/data/com.termux/files/usr/include/
+cp -r include/* $PREFIX/include/
 cd ..
 
-git clone https://github.com/svaarala/duktape --depth=1
-cd duktape
-pip2 install PyYAML
-python2 util/dist.py
-cd dist/src
-cc -c -O3 -o duktape.o duktape.c
-cc -c -O3 -o duk_module_node.o -I. ../extras/module-node/duk_module_node.c
-ar cr libduktape.a duktape.o
-ar cr libduktape_module.a duk_module_node.o
-install -m0644 ./*.a /data/data/com.termux/files/usr/lib
-install -m0644 duk*.h /data/data/com.termux/files/usr/include
-install -m0644 ../extras/module-node/duk_module_node.h /data/data/com.termux/files/usr/include
-cd ../../..
+git clone https://github.com/ftk/quickjspp --depth=1
+cd quickjspp
+cmake -DCMAKE_BUILD_TYPE=Release .
+make quickjs -j3
+install -m644 quickjs/libquickjs.a $PREFIX/lib/
+install -d $PREFIX/include/quickjs/
+install -m644 quickjs/quickjs.h quickjs/quickjs-libc.h $PREFIX/include/quickjs/
+install -m644 quickjspp.hpp $PREFIX/include/
+cd ..
+
+git clone https://github.com/PerMalmberg/libcron --depth=1
+cd libcron
+git submodule update --init
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX .
+make libcron install -j3
+cd ..
+
+git clone https://github.com/ToruNiina/toml11 --depth=1
+cd toml11
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_CXX_STANDARD=11 .
+make install -j3
+cd ..
